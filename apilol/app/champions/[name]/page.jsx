8dd98478e13ded1f06cd701207fe7b5dc3404091
GABRIEL GONCALVES ALBANEZ <gabriel.galbanez@senacsp.edu.br>
2023-07-19 17:15:09 -0300

    update 2

--- apilol/app/champions/[name]/page.jsx
+++ apilol/app/champions/[name]/page.jsx
@@ -7,7 +7,7 @@ async function getChampions(championsName){
     headers: {
       'Content-Type': 'application/json',
     },
-    cache : "reload"
+    cache : "force-cache"
   })
   const data = await request.json()
   return data
@@ -25,17 +25,21 @@ export default async function Champions({params}){
 };
 
   return(
-    <div className={`bg-cover object-cover  h-screen w-screen  absolute`}  style={backgroundImageStyle}  >
+    <div className="h-screen w-screen bg-zinc-900">
+    <div className={`bg-cover object-cover opacity-20  blur-md h-screen w-screen  absolute`}  style={backgroundImageStyle}  ></div>
        
         {/* <Image src={championFiltrado.img} alt={championFiltrado.name} height={2000} width={1000}  className="w-full h-2/3 object-cover aspect-square bg-center"/> */}
-        <h2 className="z-auto relative">{championFiltrado.name}</h2>
+        <div className="z-auto relative flex justify-center items-center h-2/3">
+        <div className={`bg-cover object-cover w-2/3 h-4/5 aspect-square rounded-md`}  style={backgroundImageStyle}  ></div>
+       
+        {/* <h2 className="">{championFiltrado.name}</h2>
         <p>{championFiltrado.title}</p>
         <p>{championFiltrado.type}</p>
         <h1>skinks</h1>
-        <h2>{championFiltrado.skinks.skin1.name}</h2>
-        <Image src={championFiltrado.skinks.skin1.imgSkin} alt={championFiltrado.name} height={100} width={100}/>
+        <h2>{championFiltrado.skinks.skin1.name}</h2> */}
+        </div>
 
-
+    
     </div>
  
    
